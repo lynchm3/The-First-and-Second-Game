6bdca4e85124bb826bba0b55847bc39be80ccb0a
Mark Lynch <lynchm3@tcd.ie>
2015-06-21 12:51:17 +0100

    missing file

---
+++ tactics_project/src/com/marklynch/tactics/objects/level/ActivityLog.java
@@ -0,0 +1,14 @@
+package com.marklynch.tactics.objects.level;
+
+public class ActivityLog {
+
+	String text;
+	Faction faction;
+
+	public ActivityLog(String text, Faction faction) {
+		super();
+		this.text = text;
+		this.faction = faction;
+	}
+
+}
