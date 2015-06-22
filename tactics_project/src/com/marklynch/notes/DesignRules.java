e042723e17d41897d6b43a9c44bd9786b6bcadbb
Mark Lynch <lynchm3@tcd.ie>
2015-06-22 21:53:13 +0100

    notes

---
+++ tactics_project/src/com/marklynch/notes/DesignRules.java
@@ -0,0 +1,14 @@
+package com.marklynch.notes;
+
+public class DesignRules {
+
+	// show as musch info as possible to the user, but without cluttering
+	// reasoning - like... when they don't compare weapons for you... fucking
+	// bullshit (maybe I could word this reasoning better?) :D
+
+	// add lots of subtleties and complexities as possible, but again, without
+	// it becoming cluttered ;)
+	// reasoning - subleties add depth, without the depth the game is just
+	// fucking boring
+
+}
