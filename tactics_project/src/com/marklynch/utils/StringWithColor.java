3aff08a41cf11b99272aa981c281c7b4efd58a9c
Mark Lynch <lynchm3@tcd.ie>
2015-07-01 11:39:09 +0100

    Missing file

---
+++ tactics_project/src/com/marklynch/utils/StringWithColor.java
@@ -0,0 +1,15 @@
+package com.marklynch.utils;
+
+import java.awt.Color;
+
+public class StringWithColor {
+
+	String string;
+	Color color;
+
+	public StringWithColor(String string, Color color) {
+		super();
+		this.string = string;
+		this.color = color;
+	}
+}
