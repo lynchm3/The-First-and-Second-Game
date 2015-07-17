e737448ecebed114025ad64dff1a045a13fdb565
Mark Lynch <lynchm3@tcd.ie>
2015-07-17 17:13:36 +0100

    Missing file

---
+++ tactics_project/src/com/marklynch/editor/settingswindow/ColorSettingsWindow.java
@@ -0,0 +1,104 @@
+package com.marklynch.editor.settingswindow;
+
+import org.newdawn.slick.Color;
+
+import com.marklynch.editor.AttributesWindow;
+import com.marklynch.editor.Editor;
+import com.marklynch.ui.button.ClickListener;
+import com.marklynch.ui.button.SettingsWindowButton;
+
+public class ColorSettingsWindow extends SettingsWindow {
+
+	public ColorSettingsWindow(float width, final Editor editor) {
+		super(width, editor);
+		updateColorsButtons();
+	}
+
+	public void updateColorsButtons() {
+		buttons.clear();
+
+		final SettingsWindowButton addColorButton = new SettingsWindowButton(0,
+				100, 200, 30, "ADD COLOR", true, true, this) {
+
+			@Override
+			public void keyTyped(char character) {
+			}
+
+			@Override
+			public void enterTyped() {
+			}
+
+			@Override
+			public void backTyped() {
+			}
+
+			@Override
+			public void depress() {
+			}
+
+		};
+
+		addColorButton.clickListener = new ClickListener() {
+
+			@Override
+			public void click() {
+
+				editor.colors.add(new Color(Color.red));
+				updateColorsButtons();
+				editor.clearSelectedObject();
+				editor.depressButtonsSettingsAndDetailsButtons();
+			}
+		};
+		buttons.add(addColorButton);
+
+		for (int i = 0; i < editor.colors.size(); i++) {
+			final int index = i;
+
+			final SettingsWindowButton collorButton = new SettingsWindowButton(
+					0, 200 + i * 30, 200, 30, editor.colors.get(i), true, true,
+					this) {
+
+				@Override
+				public void keyTyped(char character) {
+				}
+
+				@Override
+				public void enterTyped() {
+				}
+
+				@Override
+				public void backTyped() {
+				}
+
+				@Override
+				public void depress() {
+				}
+
+			};
+
+			collorButton.clickListener = new ClickListener() {
+
+				@Override
+				public void click() {
+					editor.clearSelectedObject();
+					editor.depressButtonsSettingsAndDetailsButtons();
+					collorButton.down = true;
+					editor.attributesWindow = new AttributesWindow(200, 200,
+							350, editor.colors.get(index), editor);
+
+				}
+			};
+			buttons.add(collorButton);
+
+		}
+
+	}
+
+	@Override
+	public void update() {
+		updateColorsButtons();
+		SettingsWindowButton button = this.getButton(editor.objectToEdit);
+		if (button != null)
+			button.down = true;
+	}
+}
