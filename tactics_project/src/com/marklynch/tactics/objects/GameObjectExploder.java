b3d7f9abe56cc9ffb99989764afa1c16ecf0caa6
Mark Lynch <lynchm3@tcd.ie>
2015-09-15 11:38:16 +0100

    Splitting in to squares, woop

--- tactics_project/src/com/marklynch/tactics/objects/GameObjectExploder.java
+++ tactics_project/src/com/marklynch/tactics/objects/GameObjectExploder.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 import com.marklynch.tactics.objects.level.Square;
 import com.marklynch.tactics.objects.weapons.Weapon;
 import com.marklynch.utils.QuadUtils;
+import com.marklynch.utils.TriangleUtils;
 
 public class GameObjectExploder extends GameObject {
 
-	public float centerPixelX;
-	public float centerPixelY;
-	public float[] edgePixelsX;
-	public float[] edgePixelsY;
-	public float pieceCount;
+	public SquarePiece[] squarePieces;
+
+	public TrianglePiece[] trianglePieces;
 
 	public GameObjectExploder(String name, int health, int strength,
 			int dexterity, int intelligence, int endurance, String imagePath,
@@ -36,37 +35,96 @@ public class GameObjectExploder extends GameObject {
 	}
 
 	public void explode() {
-		createPieces(4);
+		// createTrianglePieces(4);
+		createSquarePieces(4);
 	}
 
-	public void createPieces(int pieceCount) {
-		this.pieceCount = pieceCount;
+	public void createSquarePieces(int root) {
+		int pieceCount = root * root;
+		squarePieces = new SquarePiece[pieceCount];
+		float pieceWidth = this.imageTexture.getWidth() / root;
+		float pieceHeight = this.imageTexture.getHeight() / root;
+
+		for (float i = 0; i < root; i++) {
+			for (float j = 0; j < root; j++) {
+
+				SquarePiece squarePiece = squarePieces[(int) (i * root + j)] = new SquarePiece();
+				squarePiece.x1 = i * pieceWidth;
+				squarePiece.x2 = i * pieceWidth + pieceWidth;
+				squarePiece.x3 = i * pieceWidth + pieceWidth;
+				squarePiece.x4 = i * pieceWidth;
+				squarePiece.y1 = j * pieceHeight;
+				squarePiece.y2 = j * pieceHeight;
+				squarePiece.y3 = j * pieceHeight + pieceHeight;
+				squarePiece.y4 = j * pieceHeight + pieceHeight;
+				squarePiece.u1 = (i * pieceWidth) / imageTexture.getWidth();
+				squarePiece.u2 = (i * pieceWidth + pieceWidth)
+						/ imageTexture.getWidth();
+				squarePiece.u3 = (i * pieceWidth + pieceWidth)
+						/ imageTexture.getWidth();
+				squarePiece.u4 = (i * pieceWidth) / imageTexture.getWidth();
+				squarePiece.v1 = (j * pieceHeight) / imageTexture.getHeight();
+				squarePiece.v2 = (j * pieceHeight) / imageTexture.getHeight();
+				squarePiece.v3 = (j * pieceHeight + pieceHeight)
+						/ imageTexture.getHeight();
+				squarePiece.v4 = (j * pieceHeight + pieceHeight)
+						/ imageTexture.getHeight();
+			}
+		}
+
+	}
+
+	// This method is dodge AF :)
+	public void createTrianglePieces(int pieceCount) {
+
+		float centerPixelX;
+		float centerPixelY;
+		float[] edgePixelsX;
+		float[] edgePixelsY;
+
 		int imageWidth = this.imageTexture.getWidth();
 		int imageHeight = this.imageTexture.getHeight();
 
-		// 1. select random pixel
-		centerPixelX = (int) (Math.random() * imageWidth);
-		centerPixelY = (int) (Math.random() * imageHeight);
+		// 1. select random pixel for center breaking point
+		centerPixelX = (float) ((Math.random() * imageWidth) / 2f + (imageWidth / 4f));
+		centerPixelY = (float) ((Math.random() * imageHeight) / 2f + (imageHeight / 4f));
 
 		// 2. select X random edge pixels
 		edgePixelsX = new float[pieceCount];
 		edgePixelsY = new float[pieceCount];
 		for (int i = 0; i < pieceCount; i++) {
-			if (i % 4 == 0) {
+			if (i % pieceCount == 0) {
 				edgePixelsX[i] = 0;
 				edgePixelsY[i] = (int) (Math.random() * imageHeight);
-			} else if (i % 4 == 1) {
+			} else if (i % pieceCount == 1) {
 				edgePixelsX[i] = (int) (Math.random() * imageWidth);
 				edgePixelsY[i] = 0;
-			} else if (i % 4 == 2) {
+			} else if (i % pieceCount == 2) {
 				edgePixelsX[i] = imageWidth - 1;
 				edgePixelsY[i] = (int) (Math.random() * imageHeight);
-			} else if (i % 4 == 3) {
+			} else if (i % pieceCount == 3) {
 				edgePixelsX[i] = (int) (Math.random() * imageWidth);
 				edgePixelsY[i] = imageHeight - 1;
 			}
 		}
 
+		// 3. Create triangles
+		trianglePieces = new TrianglePiece[pieceCount];
+		for (int i = 0; i < pieceCount; i++) {
+			trianglePieces[i] = new TrianglePiece();
+			trianglePieces[i].x1 = edgePixelsX[i] + i * 120f;
+			trianglePieces[i].x2 = centerPixelX + i * 120f;
+			trianglePieces[i].x3 = edgePixelsX[0] + i * 120f;
+			trianglePieces[i].y1 = edgePixelsY[i];
+			trianglePieces[i].y2 = centerPixelY;
+			trianglePieces[i].y3 = edgePixelsY[0];
+			trianglePieces[i].u1 = edgePixelsX[i] / imageTexture.getWidth();
+			trianglePieces[i].u2 = centerPixelX / imageTexture.getWidth();
+			trianglePieces[i].u3 = edgePixelsX[0] / imageTexture.getWidth();
+			trianglePieces[i].v1 = edgePixelsY[i] / imageTexture.getHeight();
+			trianglePieces[i].v2 = centerPixelY / imageTexture.getHeight();
+			trianglePieces[i].v3 = edgePixelsY[0] / imageTexture.getHeight();
+		}
 	}
 
 	@Override
@@ -82,108 +140,65 @@ public class GameObjectExploder extends GameObject {
 			return;
 		}
 
-		float x1 = 0;
-		float x2 = 120;
-		float x3 = 120;
-		float x4 = 0;
-		float y1 = 0;
-		float y2 = 0;
-		float y3 = 120;
-		float y4 = 120;
-		float u1 = 0;
-		float u2 = 0.5f;
-		float u3 = 0.5f;
-		float u4 = 0;
-		float v1 = 0;
-		float v2 = 0;
-		float v3 = 0.5f;
-		float v4 = 0.5f;
-		QuadUtils.drawQuad(imageTexture, x1, x2, x3, x4, y1, y2, y3, y4, u1,
-				u2, u3, u4, v1, v2, v3, v4);
+		for (int i = 0; trianglePieces != null && i < trianglePieces.length; i++) {
 
-		// System.out.println("Triangles " + 0 + " - x1 = " + x1 + ", x2 = " +
-		// x2
-		// + ", x3 = " + x3 + ", y1 = " + y1 + ", y2 = " + y2 + ", y3 = "
-		// + y3 + ", u1 = " + u1 + ", u2 = " + u2 + ", u3 = " + u3
-		// + ", v1 = " + v1 + ", v2 = " + v2 + ", v3 = " + v3);
-		//
-		// for (int i = 0; i < pieceCount; i++) {
-		// if (i == pieceCount - 1) {
-		//
-		// float x1 = edgePixelsX[i] + i * 120f;
-		// float x2 = centerPixelX + i * 120f;
-		// float x3 = edgePixelsX[0] + i * 120f;
-		// float y1 = edgePixelsY[i];
-		// float y2 = centerPixelY;
-		// float y3 = edgePixelsY[0];
-		// float u1 = edgePixelsX[i] / imageTexture.getWidth();
-		// float u2 = centerPixelX / imageTexture.getWidth();
-		// float u3 = edgePixelsX[0] / imageTexture.getWidth();
-		// float v1 = edgePixelsY[i] / imageTexture.getHeight();
-		// float v2 = centerPixelY / imageTexture.getHeight();
-		// float v3 = edgePixelsY[0] / imageTexture.getHeight();
-		//
-		// TriangleUtils.drawTriangle(imageTexture, x1, x2, x3, y1, y2,
-		// y3, u1, u2, u3, v1, v2, v3);
-		//
-		// System.out.println("Triangles " + i + " - x1 = " + x1
-		// + ", x2 = " + x2 + ", x3 = " + x3 + ", y1 = " + y1
-		// + ", y2 = " + y2 + ", y3 = " + y3 + ", u1 = " + u1
-		// + ", u2 = " + u2 + ", u3 = " + u3 + ", v1 = " + v1
-		// + ", v2 = " + v2 + ", v3 = " + v3);
-		//
-		// } else {
-		//
-		// float x1 = edgePixelsX[i] + i * 120f;
-		// float x2 = centerPixelX + i * 120f;
-		// float x3 = edgePixelsX[i + 1] + i * 120f;
-		// float y1 = edgePixelsY[i];
-		// float y2 = centerPixelY;
-		// float y3 = edgePixelsY[i + 1];
-		// float u1 = edgePixelsX[i] / imageTexture.getWidth();
-		// float u2 = centerPixelX / imageTexture.getWidth();
-		// float u3 = edgePixelsX[i + 1] / imageTexture.getWidth();
-		// float v1 = edgePixelsY[i] / imageTexture.getHeight();
-		// float v2 = centerPixelY / imageTexture.getHeight();
-		// float v3 = edgePixelsY[i + 1] / imageTexture.getHeight();
-		// TriangleUtils.drawTriangle(imageTexture, x1, x2, x3, y1, y2,
-		// y3, u1, u2, u3, v1, v2, v3);
-		//
-		// System.out.println("Triangles " + i + " - x1 = " + x1
-		// + ", x2 = " + x2 + ", x3 = " + x3 + ", y1 = " + y1
-		// + ", y2 = " + y2 + ", y3 = " + y3 + ", u1 = " + u1
-		// + ", u2 = " + u2 + ", u3 = " + u3 + ", v1 = " + v1
-		// + ", v2 = " + v2 + ", v3 = " + v3);
-		// }
-		// }
+			if (trianglePieces[i] == null)
+				continue;
 
-		// draw the pieces at 0,0
-		// Game.activeBatch.dr
+			TriangleUtils.drawTriangle(imageTexture, trianglePieces[i].x1,
+					trianglePieces[i].x2, trianglePieces[i].x3,
+					trianglePieces[i].y1, trianglePieces[i].y2,
+					trianglePieces[i].y3, trianglePieces[i].u1,
+					trianglePieces[i].u2, trianglePieces[i].u3,
+					trianglePieces[i].v1, trianglePieces[i].v2,
+					trianglePieces[i].v3);
+		}
 
-		// Draw object
-		// int actorPositionXInPixels = this.squareGameObjectIsOn.x
-		// * (int) Game.SQUARE_WIDTH;
-		// int actorPositionYInPixels = this.squareGameObjectIsOn.y
-		// * (int) Game.SQUARE_HEIGHT;
-		//
-		// float alpha = 1.0f;
-		// if (Game.level.activeActor != null
-		// && Game.level.activeActor.showHoverFightPreview == true
-		// && Game.level.activeActor.hoverFightPreviewDefender != this) {
-		// alpha = 0.5f;
-		// }
-		//
-		// if (hasAttackedThisTurn == true && this.faction != null
-		// && Game.level.currentFactionMoving == this.faction) {
-		// alpha = 0.5f;
-		// }
-		//
-		// TextureUtils.skipNormals = true;
-		//
-		// TextureUtils.drawTexture(imageTexture, alpha, actorPositionXInPixels,
-		// actorPositionXInPixels + Game.SQUARE_WIDTH,
-		// actorPositionYInPixels, actorPositionYInPixels
-		// + Game.SQUARE_HEIGHT);
-		// TextureUtils.skipNormals = false;
+		for (int i = 0; squarePieces != null && i < squarePieces.length; i++) {
+
+			if (squarePieces[i] == null)
+				continue;
+
+			QuadUtils.drawQuad(imageTexture, squarePieces[i].x1,
+					squarePieces[i].x2, squarePieces[i].x3, squarePieces[i].x4,
+					squarePieces[i].y1, squarePieces[i].y2, squarePieces[i].y3,
+					squarePieces[i].y4, squarePieces[i].u1, squarePieces[i].u2,
+					squarePieces[i].u3, squarePieces[i].u4, squarePieces[i].v1,
+					squarePieces[i].v2, squarePieces[i].v3, squarePieces[i].v4);
+		}
+	}
+
+	public static class SquarePiece {
+		float x1;
+		float x2;
+		float x3;
+		float x4;
+		float y1;
+		float y2;
+		float y3;
+		float y4;
+		float u1;
+		float u2;
+		float u3;
+		float u4;
+		float v1;
+		float v2;
+		float v3;
+		float v4;
+	}
+
+	public static class TrianglePiece {
+		float x1;
+		float x2;
+		float x3;
+		float y1;
+		float y2;
+		float y3;
+		float u1;
+		float u2;
+		float u3;
+		float v1;
+		float v2;
+		float v3;
 	}
 }
