007e9b47aef2a5ce5504e5519ca25f4d7ad7018d
lynchm3 <lynchm3@tcd.ie>
2017-02-21 16:52:07 +0000

    missing

--- tactics_project/src/com/marklynch/objects/weapons/Weapon.java
+++ tactics_project/src/com/marklynch/objects/weapons/Weapon.java
@@ -3,6 +3,7 @@ package com.marklynch.objects.weapons;
 import java.util.Vector;
 
 import com.marklynch.level.Square;
+import com.marklynch.objects.Inventory;
 import com.marklynch.objects.Owner;
 import com.marklynch.objects.units.Actor.Direction;
 
@@ -11,7 +12,8 @@ public class Weapon extends WeaponTemplate {
 	public Owner owner;
 
 	public Weapon(String name, float damage, float minRange, float maxRange, String imagePath, float health,
-			Square squareGameObjectIsOn, boolean fitsInInventory, boolean canContainOtherObjects, float widthRatio, float heightRatio) {
+			Square squareGameObjectIsOn, boolean fitsInInventory, boolean canContainOtherObjects, float widthRatio,
+			float heightRatio) {
 		super(name, damage, minRange, maxRange, imagePath, health, squareGameObjectIsOn, fitsInInventory,
 				canContainOtherObjects, widthRatio, heightRatio);
 
@@ -22,7 +24,7 @@ public class Weapon extends WeaponTemplate {
 		for (int i = 0; i < squares.length; i++) {
 			for (int j = 0; j < squares[0].length; j++) {
 				if (squares[i][j].reachableBySelectedCharater) {
-					for (float range = minRange; range <= maxRange; range++) {
+					for (float range = getEffectiveMinRange(); range <= getEffectiveMaxRange(); range++) {
 						Vector<Square> squaresInThisPath = new Vector<Square>();
 						squaresInThisPath.add(squares[i][j]);
 						calculateAttackableSquares(squares, range, squares[i][j], Direction.UP, squaresInThisPath);
@@ -75,9 +77,56 @@ public class Weapon extends WeaponTemplate {
 	}
 
 	public boolean hasRange(int weaponDistanceTo) {
-		if (weaponDistanceTo >= minRange && weaponDistanceTo <= maxRange) {
+		if (weaponDistanceTo >= getEffectiveMinRange() && weaponDistanceTo <= getEffectiveMaxRange()) {
 			return true;
 		}
 		return false;
+	}
+
+	public int compareWeaponToWeapon(Weapon otherGameObject) {
+
+		System.out.println("compareWeaponToWeapon");
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_TOTAL_DAMAGE) {
+			return Math.round(otherGameObject.getTotalDamage() - this.getTotalDamage());
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_SLASH_DAMAGE) {
+			return Math.round(otherGameObject.slashDamage - this.slashDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_BLUNT_DAMAGE) {
+			return Math.round(otherGameObject.bluntDamage - this.bluntDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_PIERCE_DAMAGE) {
+			return Math.round(otherGameObject.pierceDamage - this.pierceDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_FIRE_DAMAGE) {
+			return Math.round(otherGameObject.fireDamage - this.fireDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_WATER_DAMAGE) {
+			return Math.round(otherGameObject.waterDamage - this.waterDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_POISON_DAMAGE) {
+			return Math.round(otherGameObject.electricalDamage - this.electricalDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_ELECTRICAL_DAMAGE) {
+			return Math.round(otherGameObject.poisonDamage - this.poisonDamage);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_MAX_RANGE) {
+			return Math.round(otherGameObject.maxRange - this.maxRange);
+		}
+
+		if (Inventory.inventorySortBy == Inventory.INVENTORY_SORT_BY.SORT_BY_MIN_RANGE) {
+			return Math.round(otherGameObject.minRange - this.minRange);
+		}
+
+		return 0;
+
 	}
 }
--- tactics_project/src/com/marklynch/objects/weapons/WeaponTemplate.java
+++ tactics_project/src/com/marklynch/objects/weapons/WeaponTemplate.java
@@ -10,24 +10,77 @@ public class WeaponTemplate extends GameObject {
 			"canContainOtherObjects" };
 
 	// attributes
-	public float damage = 0;
+	public float slashDamage = 0;
+	public float pierceDamage = 0;
+	public float bluntDamage = 0;
+	public float fireDamage = 0; // fire/purify/clean
+	public float waterDamage = 0; // water/life
+	public float electricalDamage = 0; // lightning/light/electrical/speed
+	public float poisonDamage = 0;// poison/ground/contaminate/neutralize/slow/corruption
 	public float minRange = 0;
 	public float maxRange = 0;
 
 	public WeaponTemplate(String name, float damage, float minRange, float maxRange, String imagePath, float health,
-			Square squareGameObjectIsOn, boolean fitsInInventory, boolean canContainOtherObjects, float widthRatio, float heightRatio) {
+			Square squareGameObjectIsOn, boolean fitsInInventory, boolean canContainOtherObjects, float widthRatio,
+			float heightRatio) {
 
 		super(name, (int) health, imagePath, squareGameObjectIsOn, new Inventory(), false, true, fitsInInventory,
 				canContainOtherObjects, widthRatio, heightRatio);
 
-		this.damage = damage;
+		this.slashDamage = damage;
 		this.minRange = minRange;
 		this.maxRange = maxRange;
 	}
 
+	public float getEffectiveSlashDamage() {
+		return slashDamage;
+	}
+
+	public float getEffectivePierceDamage() {
+		return pierceDamage;
+	}
+
+	public float getEffectiveBluntDamage() {
+		return bluntDamage;
+	}
+
+	public float getEffectiveFireDamage() {
+		return fireDamage;
+	}
+
+	public float getEffectiveWaterDamage() {
+		return waterDamage;
+	}
+
+	public float getEffectiveElectricalDamage() {
+		return electricalDamage;
+	}
+
+	public float getEffectivePoisonDamage() {
+		return poisonDamage;
+	}
+
+	public float getEffectiveMinRange() {
+		return minRange;
+	}
+
+	public float getEffectiveMaxRange() {
+		return maxRange;
+	}
+
+	public float getTotalDamage() {
+		return slashDamage + pierceDamage + bluntDamage + fireDamage + waterDamage + electricalDamage + poisonDamage;
+	}
+
+	public float getTotalEffectiveDamage() {
+		return getEffectiveSlashDamage() + getEffectivePierceDamage() + getEffectiveBluntDamage()
+				+ getEffectiveFireDamage() + getEffectiveWaterDamage() + getEffectiveElectricalDamage()
+				+ getEffectivePoisonDamage();
+	}
+
 	@Override
 	public GameObject makeCopy(Square square) {
-		return new Weapon(new String(name), damage, minRange, maxRange, imageTexturePath, totalHealth, square,
+		return new Weapon(new String(name), slashDamage, minRange, maxRange, imageTexturePath, totalHealth, square,
 				fitsInInventory, canContainOtherObjects, widthRatio, heightRatio);
 	}
 }
