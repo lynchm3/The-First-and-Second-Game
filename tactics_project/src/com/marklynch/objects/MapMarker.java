252011e8249740028483c5fed5dfd994aee4be54
lynchm3 <lynchm3@tcd.ie>
2017-08-02 13:42:13 +0100

    Map marker translucent, map marker points to middle of sqr.

--- tactics_project/src/com/marklynch/objects/MapMarker.java
+++ tactics_project/src/com/marklynch/objects/MapMarker.java
@@ -59,6 +59,10 @@ public class MapMarker extends GameObject {
 
 		System.out.println("MapMarker.drawStaticUI");
 
+		float alpha = 1;
+		if (Game.zoomLevelIndex < Game.MAP_MODE_ZOOM_LEVEL_INDEX)
+			alpha = 0.5f;
+
 		int squarePositionX1 = this.squareGameObjectIsOn.xInGrid * (int) Game.SQUARE_WIDTH;
 		int squarePositionY1 = this.squareGameObjectIsOn.yInGrid * (int) Game.SQUARE_HEIGHT;
 		float drawPositionX1 = (Game.windowWidth / 2)
@@ -66,10 +70,11 @@ public class MapMarker extends GameObject {
 				- (Game.HALF_SQUARE_WIDTH - Game.HALF_SQUARE_WIDTH * Game.zoom);
 		float drawPositionY1 = (Game.windowHeight / 2)
 				+ (Game.zoom * (squarePositionY1 - Game.windowHeight / 2 + Game.dragY))
-				- (Game.SQUARE_HEIGHT - Game.SQUARE_HEIGHT * Game.zoom);
+				- (Game.SQUARE_HEIGHT - Game.SQUARE_HEIGHT * Game.zoom) - (Game.HALF_SQUARE_HEIGHT * Game.zoom);
+		;
 		float drawPositionX2 = drawPositionX1 + (int) Game.SQUARE_WIDTH;
 		float drawPositionY2 = drawPositionY1 + (int) Game.SQUARE_HEIGHT;
-		TextureUtils.drawTexture(imageTexture, drawPositionX1, drawPositionY1, drawPositionX2, drawPositionY2);
+		TextureUtils.drawTexture(imageTexture, alpha, drawPositionX1, drawPositionY1, drawPositionX2, drawPositionY2);
 	}
 
 	@Override
