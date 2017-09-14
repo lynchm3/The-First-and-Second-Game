816bd2afffb1020029f29eecd8ce3e1e04d9c59d
lynchm3 <lynchm3@tcd.ie>
2017-09-14 12:05:38 +0100

    Close button for adventure log

--- tactics_project/src/com/marklynch/level/constructs/adventurelog/AdventureLog.java
+++ tactics_project/src/com/marklynch/level/constructs/adventurelog/AdventureLog.java
@@ -7,6 +7,8 @@ import com.marklynch.level.Level;
 import com.marklynch.level.quest.Quest;
 import com.marklynch.ui.Draggable;
 import com.marklynch.ui.Scrollable;
+import com.marklynch.ui.button.Button;
+import com.marklynch.ui.button.ClickListener;
 import com.marklynch.ui.button.LevelButton;
 import com.marklynch.utils.QuadUtils;
 import com.marklynch.utils.TextUtils;
@@ -27,11 +29,26 @@ public class AdventureLog implements Draggable, Scrollable {
 	int contentY;
 	int contentBorder;
 
-	int listItemHeight = 30;
+	int listItemHeight;
+
+	transient static int bottomBorderHeight;
+
 	public ArrayList<LevelButton> buttons = new ArrayList<LevelButton>();
+	// Close button
+	static LevelButton buttonClose;
 
 	public AdventureLog() {
 		resize();
+
+		buttonClose = new LevelButton(Game.halfWindowWidth - 25f, bottomBorderHeight, 70f, 30f, "end_turn_button.png",
+				"end_turn_button.png", "CLOSE [L]", true, false, Color.BLACK, Color.WHITE);
+		buttonClose.setClickListener(new ClickListener() {
+			@Override
+			public void click() {
+				Game.level.openCloseAdventureLog();
+			}
+		});
+		buttons.add(buttonClose);
 	}
 
 	public void resize() {
@@ -45,6 +62,8 @@ public class AdventureLog implements Draggable, Scrollable {
 		contentBorder = 16;
 
 		listItemHeight = 30;
+
+		bottomBorderHeight = 384;
 
 	}
 
@@ -64,6 +83,7 @@ public class AdventureLog implements Draggable, Scrollable {
 		// Black cover
 		QuadUtils.drawQuad(Color.BLACK, 0, Game.windowWidth, 0, Game.windowHeight);
 
+		// List of quests
 		int questsDrawnInList = 0;
 		for (Quest quest : Level.quests) {
 			if (quest.started) {
@@ -80,6 +100,7 @@ public class AdventureLog implements Draggable, Scrollable {
 			}
 		}
 
+		// Content
 		int questTextsDrawn = 0;
 		if (selectedQuest != null) {
 			for (String textPart : selectedQuest.text) {
@@ -93,6 +114,11 @@ public class AdventureLog implements Draggable, Scrollable {
 		if (questsDrawnInList == 0) {
 			TextUtils.printTextWithImages(0, 0, Integer.MAX_VALUE, true, new Object[] { "NO QUESTS" });
 		} else {
+		}
+
+		// Buttons
+		for (Button button : buttons) {
+			button.draw();
 		}
 	}
 
