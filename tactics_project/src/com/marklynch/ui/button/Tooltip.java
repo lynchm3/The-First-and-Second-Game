9fb25af5daaee6a0d9a939709fc760c4cd01db3d
Mark Lynch <mark.lynch@retailinmotion.com>
2017-10-07 10:22:34 +0100

    fixed tooltip spread direction

--- tactics_project/src/com/marklynch/ui/button/Tooltip.java
+++ tactics_project/src/com/marklynch/ui/button/Tooltip.java
@@ -35,32 +35,32 @@ public class Tooltip {
 				// top left quadrant
 				x1 = levelButton.realX + levelButton.width + 10;
 				x2 = x1 + dimensions[0];
-				y2 = levelButton.realY + levelButton.height;
-				y1 = y2 - dimensions[1];
+				y1 = levelButton.realY;
+				y2 = y1 + dimensions[1];
 				;
 
 			} else if (levelButton.realX <= Game.halfWindowWidth && levelButton.realY > Game.halfWindowHeight) {
 				// bottom left quadrant
 				x1 = levelButton.realX + levelButton.width + 10;
 				x2 = x1 + dimensions[0];
-				y1 = levelButton.realY;
-				y2 = y1 + dimensions[1];
+				y2 = levelButton.realY + levelButton.height;
+				y1 = y2 - dimensions[1];
 				;
 
 			} else if (levelButton.realX > Game.halfWindowWidth && levelButton.realY <= Game.halfWindowHeight) {
+				// bottom right
+				x2 = levelButton.realX + -10;
+				x1 = x2 - dimensions[0];
+				y1 = levelButton.realY;
+				y2 = y1 + dimensions[1];
+
+			} else if (levelButton.realX > Game.halfWindowWidth && levelButton.realY > Game.halfWindowHeight) {
 				// top right
 				x2 = levelButton.realX + -10;
 				x1 = x2 - dimensions[0];
 				y2 = levelButton.realY + levelButton.height;
 				y1 = y2 - dimensions[1];
 				;
-
-			} else if (levelButton.realX > Game.halfWindowWidth && levelButton.realY > Game.halfWindowHeight) {
-				// bottom right
-				x2 = levelButton.realX + -10;
-				x1 = x2 - dimensions[0];
-				y1 = levelButton.realY;
-				y2 = y1 + dimensions[1];
 
 			}
 
