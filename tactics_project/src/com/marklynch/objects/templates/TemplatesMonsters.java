d4349c192dac5974894d418d222a7d7c3ec53e3c
Mark Lynch <mark.lynch@retailinmotion.com>
2017-11-07 11:45:44 +0000

    Getting it to run...

--- tactics_project/src/com/marklynch/objects/templates/TemplatesMonsters.java
+++ tactics_project/src/com/marklynch/objects/templates/TemplatesMonsters.java
@@ -1,8 +1,10 @@
 package com.marklynch.objects.templates;
 
+import com.marklynch.ai.routines.AIRoutineForRockGolem;
 import com.marklynch.level.quest.caveoftheblind.AIRoutineForBlind;
 import com.marklynch.level.quest.caveoftheblind.Blind;
 import com.marklynch.objects.GameObject;
+import com.marklynch.objects.units.RockGolem;
 
 public class TemplatesMonsters {
 
@@ -26,6 +28,23 @@ public class TemplatesMonsters {
 		Templates.BLIND.canEquipWeapons = true;
 		Templates.BLIND.templateId = GameObject.generateNewTemplateId();
 		Templates.BLIND.aiRoutine = new AIRoutineForBlind(Templates.BLIND);
+
+		Templates.ROCK_GOLEM = new RockGolem();
+		Templates.ROCK_GOLEM.title = "Suspicious Boulder";
+		Templates.ROCK_GOLEM.actorLevel = 1;
+		Templates.ROCK_GOLEM.totalHealth = Templates.ROCK_GOLEM.remainingHealth = 300;
+		Templates.ROCK_GOLEM.strength = 10;
+		Templates.ROCK_GOLEM.dexterity = 10;
+		Templates.ROCK_GOLEM.intelligence = 10;
+		Templates.ROCK_GOLEM.endurance = 10;
+		Templates.ROCK_GOLEM.imageTexturePath = "rock_golem_sleeping.png";
+		Templates.ROCK_GOLEM.heightRatio = 1f;
+		Templates.ROCK_GOLEM.drawOffsetY = 1f;
+		Templates.ROCK_GOLEM.weight = 210f;
+		Templates.ROCK_GOLEM.canOpenDoors = false;
+		Templates.ROCK_GOLEM.canEquipWeapons = false;
+		Templates.ROCK_GOLEM.templateId = GameObject.generateNewTemplateId();
+		Templates.ROCK_GOLEM.aiRoutine = new AIRoutineForRockGolem(Templates.ROCK_GOLEM);
 
 		// ROCK_GOLEM = new RockGolem("Rock Golem", "Rock Golem", 1, 100, 0, 0,
 		// 0, 0, "rock_golem.png", null, 1, 10, null,
