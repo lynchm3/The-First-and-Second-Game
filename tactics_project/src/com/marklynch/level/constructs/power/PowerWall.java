a0849ed3e6c63b06d9150a55869948539c9db31a
Mark Lynch <mark.lynch@retailinmotion.com>
2019-02-05 12:18:05 +0000

    Create wall power

--- tactics_project/src/com/marklynch/level/constructs/power/PowerWall.java
+++ tactics_project/src/com/marklynch/level/constructs/power/PowerWall.java
@@ -4,25 +4,23 @@ import org.lwjgl.util.Point;
 
 import com.marklynch.actions.Action;
 import com.marklynch.level.constructs.Crime;
-import com.marklynch.level.constructs.animation.primary.AnimationPush;
 import com.marklynch.level.constructs.effect.Effect;
-import com.marklynch.level.constructs.effect.EffectBurning;
 import com.marklynch.level.squares.Square;
 import com.marklynch.objects.inanimateobjects.GameObject;
+import com.marklynch.objects.templates.Templates;
 import com.marklynch.utils.ResourceUtils;
 
 public class PowerWall extends Power {
 
-	private static String NAME = "Ignite";
+	private static String NAME = "Wall";
 
 	public PowerWall() {
 		this(null);
 	}
 
 	public PowerWall(GameObject source) {
-		super(NAME, ResourceUtils.getGlobalImage("action_burn.png", false), source,
-				new Effect[] { new EffectBurning(source, null, 3) }, 5, null, new Point[] { new Point(0, 0) }, 10, true,
-				true, Crime.TYPE.CRIME_ASSAULT);
+		super(NAME, ResourceUtils.getGlobalImage("wall_cave.png", false), source, new Effect[] {}, 5, null,
+				new Point[] { new Point(0, 0) }, 10, true, true, Crime.TYPE.CRIME_ASSAULT);
 		selectTarget = true;
 	}
 
@@ -33,8 +31,21 @@ public class PowerWall extends Power {
 
 	@Override
 	public void cast(final GameObject source, GameObject targetGameObject, Square targetSquare, final Action action) {
-		source.setPrimaryAnimation(new AnimationPush(source, targetSquare, source.getPrimaryAnimation(), null));
-		super.cast(source, targetGameObject, targetSquare, action);
+//		source.setPrimaryAnimation(new AnimationPush(source, targetSquare, source.getPrimaryAnimation(), null));
+//		super.cast(source, targetGameObject, targetSquare, action);
+
+		targetSquare.inventory.add(Templates.VEIN.makeCopy(targetSquare, null, false, Templates.ORE, 0.1f));
 
 	}
+
+	@Override
+	public boolean check(GameObject source, Square targetSquare) {
+		if (targetSquare.inventory.canShareSquare)
+			return true;
+
+		disabledReason = "Theres a " + targetSquare.inventory.gameObjectThatCantShareSquare.name + " there!";
+		return false;
+
+	}
+
 }
