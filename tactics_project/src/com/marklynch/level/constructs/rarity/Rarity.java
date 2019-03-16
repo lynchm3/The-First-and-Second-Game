1d2693c60ddbe925aa22018a35a379fd00f8baee
Mark Lynch <mark.lynch@retailinmotion.com>
2019-03-16 20:33:40 +0000

    Set colors for rarity

--- tactics_project/src/com/marklynch/level/constructs/rarity/Rarity.java
+++ tactics_project/src/com/marklynch/level/constructs/rarity/Rarity.java
@@ -5,9 +5,9 @@ import com.marklynch.utils.Color;
 public class Rarity {
 
 	public final static Rarity GOD = new Rarity(1, Color.WHITE);
-	public final static Rarity LEGEND = new Rarity(2, new Color(241, 236, 111));
-	public final static Rarity HERO = new Rarity(3, Color.GREEN);
-	public final static Rarity FIGHTER = new Rarity(4, Color.BLUE);
+	public final static Rarity LEGEND = new Rarity(2, new Color(241, 236, 111)); // light yellow
+	public final static Rarity HERO = new Rarity(3, new Color(103, 184, 84)); // mid green
+	public final static Rarity FIGHTER = new Rarity(4, new Color(65, 81, 141)); // dark blue
 	public final static Rarity COMMON = new Rarity(5, Color.DARK_GRAY);
 
 	// a nice red 240,122,121
